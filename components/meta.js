71cfdf97ce7cc9b86d417d1a497a4d3552264766
hitto-js <68594072+hitto-hub@users.noreply.github.com>
2023-04-13 13:21:48 +0000

    add Desc

--- components/meta.js
+++ components/meta.js
@@ -5,16 +5,20 @@ import { siteMeta } from "lib/constants";
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
   siteMeta;
 
-export default function Meta({ pageTitle }) {
-  // ページタイトルがない場合はサイトタイトルのみを表示
+export default function Meta({ pageTitle, pageDesc }) {
+  // タイトル
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
-
+  // 説明
+  const desc = pageDesc ?? siteDesc;
   return (
     <Head>
       <title>
         {title}
       </title>
       <meta property="og:title" content={title} />
+
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
     </Head>
   );
 }
