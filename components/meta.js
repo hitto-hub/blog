120e5cb03913d0913029e5bdc7969ac0680b426b
hitto-js <68594072+hitto-hub@users.noreply.github.com>
2023-04-13 13:29:59 +0000

    add other meta data

--- components/meta.js
+++ components/meta.js
@@ -24,6 +24,13 @@ export default function Meta({ pageTitle, pageDesc }) {
 
       <link rel="canonical" href={url} />
       <meta property="og:url" content={url} />
+
+      <meta priority="og:site_name" content={siteTitle} />
+      <meta priority="og:type" content={siteType} />
+      <meta priority="og:locale" content={siteLocale} />
+
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
     </Head>
   );
 }
