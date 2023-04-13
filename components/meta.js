77ba5c4bfdee7668d4923067eb939ae60024dab0
hitto-js <68594072+hitto-hub@users.noreply.github.com>
2023-04-13 13:13:11 +0000

    add ページタイトルメタデータ

---
+++ components/meta.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+// サイトに関する情報
+import { siteMeta } from "lib/constants";
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+  siteMeta;
+
+export default function Meta({ pageTitle }) {
+  // ページタイトルがない場合はサイトタイトルのみを表示
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+
+  return (
+    <Head>
+      <title>
+        {title}
+      </title>
+      <meta property="og:title" content={title} />
+    </Head>
+  );
+}
